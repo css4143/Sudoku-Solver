eee86cb4d99dbbc2031b1ecb585df336ac0e47c6
Connor Seiden <bullshark_82@yahoo.com>
2015-02-24 16:20:56 -0500

    Create SudokuSolver.java

---
+++ SudokuSolver.java
@@ -0,0 +1,35 @@
+
+/**
+ * The main class of the Sudoku-Solver program.
+ * 
+ * @author Connor Seiden
+ * @version 2-24-2014
+ *
+ */
+public class SudokuSolver {
+	
+	/**
+	 * The main method.  Plans to use args as inputs for a board.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args){
+		Board board = new Board();
+		Solver s = new Solver();
+		
+		long start = System.currentTimeMillis();
+		Board solution = s.solution(board);
+		long end = System.currentTimeMillis();
+		
+		
+		if(solution!=null){
+			//print solution
+		}
+		else{
+			System.out.println("unsolvable");
+		}
+		System.out.print("\nRuntime for Sudoku: "+ (end-start) +" milliseconds");
+		
+	}
+
+}
